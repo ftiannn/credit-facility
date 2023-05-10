f091da22d90b0f48c84c8e073f4df6d22ab8667b
FT Tan <fangtiannn@gmail.com>
2023-05-10 21:05:36 +0800

    first commit

---
+++ src/main/java/com/example/clientFacility/ClientFacilityApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientFacility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientFacilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientFacilityApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/clientFacility/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.clientFacility.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @PostMapping("/login")
+    public String Login() {
+        return "Hello";
+    }
+
+}
